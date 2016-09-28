b742ac16abea41c3dd404e2e0ccbb9ea7fb600b4
grswdev <anagnostopoulos.george@gmail.com>
2016-09-28 11:57:15 +0300

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # planet-names
+
+Planet and Satellite Names and Discoverers taken from http://planetarynames.wr.usgs.gov/Page/Planets (09/28/2016)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//Planet and Satellite Names and Discoverers taken from http://planetarynames.wr.usgs.gov/Page/Planets (09/28/2016)
 var planetNames = require('./planetNames.json');
 var uniqueRandomArray = require('unique-random-array');
 
